d055b3b58565e40463ad1bf10e82f3b5fc274229
GlassesArms <glassesarms@gmail.com>
2023-12-09 20:42:56 +1030

    first view and some custom css

---
+++ Server/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GlassCommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,4 @@
+global using GlassCommerce.Shared;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
