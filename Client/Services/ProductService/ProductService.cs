12cc5911fb3265e6758cd68a1b91bf04dd621a20
GlassesArms <glassesarms@gmail.com>
2023-12-10 22:20:05 +1030

    added categoryUrl has optional parameter to getproducts client side, added event subscription to the product list

--- Client/Services/ProductService/ProductService.cs
+++ Client/Services/ProductService/ProductService.cs
@@ -4,19 +4,25 @@
     {
         private readonly HttpClient _http;
 
+        public event Action ProductsChanged;
+
         public ProductService(HttpClient http)
         {
             _http = http;
         }
         public List<Product> Products { get; set; } = new List<Product>();
 
-        public async Task GetProducts()
+        public async Task GetProducts(string? categoryUrl = null)
         {
-            var result =
-                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            var result = categoryUrl == null ?
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product") :
+                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
+
             if (result != null && result.Data != null) {
                 Products = result.Data;
             }
+
+            ProductsChanged.Invoke();
         }
 
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
