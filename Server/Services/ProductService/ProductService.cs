24ead3c31021ca7741717028d42dbd117aff8120
GlassesArms <glassesarms@gmail.com>
2023-12-10 23:06:09 +1030

    added product variants to UI elements

--- Server/Services/ProductService/ProductService.cs
+++ Server/Services/ProductService/ProductService.cs
@@ -13,7 +13,7 @@
         {
             var response = new ServiceResponse<List<Product>>
             {
-                Data = await _context.Products.ToListAsync()
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
             };
 
             return response;
@@ -22,7 +22,10 @@
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if(product == null)
             {
                 response.Success = false;
@@ -42,6 +45,7 @@
             {
                 Data = await _context.Products
                     .Where(p => p.Category.Url.ToLower().Equals(categoryUrl))
+                    .Include(p => p.Variants)
                     .ToListAsync()
             };
 
