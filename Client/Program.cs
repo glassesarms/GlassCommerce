32b7c0b72c41b4ee92d8e88ce97396dd7b175053
GlassesArms <glassesarms@gmail.com>
2023-12-12 23:47:22 +1030

    #55 #56 #57

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,6 +7,7 @@ using GlassCommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
+using GlassCommerce.Client.Services.CartService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -16,5 +17,6 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
 
 await builder.Build().RunAsync();
---
+++ Client/Services/CartService/ICartService.cs
@@ -0,0 +1,9 @@
+﻿namespace GlassCommerce.Client.Services.CartService
+{
+    public interface ICartService
+    {
+        event Action OnChange;
+        Task AddToCart(CartItem cartItem);
+        Task<List<CartItem>> GetCartItems();
+    }
+}
