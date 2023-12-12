6976fe77c12944a867de33d347e58d3afdea2003
GlassesArms <glassesarms@gmail.com>
2023-12-12 19:18:26 +1030

    #53 #54

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,11 +6,13 @@ global using GlassCommerce.Client.Services.CategoryService;
 using GlassCommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
